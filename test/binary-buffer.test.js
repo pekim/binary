f72ab13e3239ad3df413b8624bafd44fb6ec1a88
Mike D Pilsbury <mike.pilsbury@gmail.com>
2010-12-11 15:27:46 +0000

    Able to add bytes to the binary buffer, and build the resulting buffer.

--- test/binary-buffer.test.js
+++ test/binary-buffer.test.js
@@ -2,5 +2,17 @@ var BinaryBuffer = require('../lib/binary-buffer').BinaryBuffer;
 
 exports.BinaryBuffer = function(test) {
   test.ok(BinaryBuffer);
+
+  test.done();
+};
+
+exports.oneByte = function(test) {
+  var bb = new BinaryBuffer();
+  bb.addByte(123);
+  var b = bb.getBuffer();
+  
+  test.equals(b.length, 1);
+  test.equals(b[0], 123);
+
   test.done();
 };
